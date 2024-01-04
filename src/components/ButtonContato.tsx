5d663a3b3693c83b2c723cabb009e44bfe9aa037
João <joaovitorromanoteixeira@gmail.com>
2024-01-04 16:07:17 -0300

    :ok_hand: style: add and remove animations

--- src/components/ButtonContato.tsx
+++ src/components/ButtonContato.tsx
@@ -18,8 +18,8 @@ export default function ButtonContato({text, icon, id, link} :ButtonProps) {
     }
     return(
         <>
-        <motion.button className="p-2 border-2 border-black rounded-2xl flex items-center text-lg sm:text-xl
-        hover:text-white hover:border-white"
+        <motion.button className="p-2 border-2 border-black rounded-2xl flex items-center w-32 justify-center  text-lg sm:text-xl
+        hover:text-white hover:border-white "
         key={id}
         whileHover={{scale: 1.1}}
         whileTap={{scale: 0.9}}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import { motion } from "framer-motion"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { IoMenu } from "react-icons/io5";
+import MenuModal from "./MenuModal";
 
 
 
 export default function Header() {
-
+    const [openModal, setOpenModal ] = useState<Boolean>(false);
     return(
         <header className="w-full h-16 fixed flex justify-center">
-            <motion.ul className="mt-5 flex gap-4 text-xl text-white"
+            <motion.ul className="mt-5 hidden gap-4 text-xl text-white sm:flex"
             initial={{y:-50, opacity:0}}
             whileInView={{y:0, opacity:1}}
             transition={{ease: "easeOut", duration: 0.5, delay: 0.5}}
@@ -17,6 +20,12 @@ export default function Header() {
                 <li><Link to="/projetos">Projetos</Link></li>
                 <li><Link to="/sobre">Sobre mim</Link></li>
             </motion.ul>
+            <div onClick={() => setOpenModal(!openModal)}
+            className="absolute right-2 mt-2 text-white text-4xl sm:hidden"
+            >
+                <IoMenu />
+                {openModal && <MenuModal />}
+            </div>
         </header>
     )
 }
